548471bca49f090b3051477fd35cd4c31aaf549e
Manoj Sharma <msharma@anystr.com>
2022-11-17 14:13:41 +0530

    hds

--- Samplelibrary/src/main/java/com/kot/samplelibrary/OpenActivity.java
+++ Samplelibrary/src/main/java/com/kot/samplelibrary/OpenActivity.java
@@ -3,11 +3,15 @@ package com.kot.samplelibrary;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 
 public class OpenActivity extends Intent {
     void  start(Context context){
         context.startActivity(this,Dashboard.class);
+    }
+    public  static void Toaster(Context context,String msg){
+        Toast.makeText(context,msg,Toast.LENGTH_LONG).show();
     }
 
 
